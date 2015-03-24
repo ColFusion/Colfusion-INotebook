b9a92ce5937dca3dbdb3aaf5a9945d80867034e4
ianhu92 <ianhu92@gmail.com>
2015-03-24 19:56:07 -0400

    update notebook.sql

--- notebook.sql
+++ notebook.sql
@@ -1,15 +1,8 @@
-
 -- Database: `notebook`
---
-
 CREATE DATABASE IF NOT EXISTS `notebook`; 
 USE `notebook`;
--- --------------------------------------------------------
 
---
--- Table `user`
---
-
+-- Table `user` 
 CREATE TABLE IF NOT EXISTS `users` (
   `userid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
@@ -17,9 +10,32 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB;
-CREATE FULLTEXT INDEX `usernameIndex` ON `users` (username);
--- --------------------------------------------------------
+
+-- Create index if exists
+DROP PROCEDURE IF EXISTS create_index;
+DELIMITER $
+CREATE PROCEDURE create_index()  
+BEGIN  
+  SELECT COUNT(*) INTO @cnt FROM information_schema.statistics WHERE table_name='users' AND index_name='usernameIndex' ;  
+  IF @cnt =0 THEN   
+    CREATE INDEX `usernameIndex` ON `users` (username);
+  END IF;
+END 
+$
+DELIMITER ;
+CALL create_index();
 
 -- Create user 'notebook'
-create user 'notebook'@'localhost' identified by 'notebook';
+DROP PROCEDURE IF EXISTS create_user;
+DELIMITER $
+CREATE PROCEDURE create_user()  
+BEGIN
+	SELECT COUNT(*) INTO @cnt FROM mysql.user WHERE user = 'notebook' and host='localhost';
+	IF @cnt =0 THEN
+		create user 'notebook'@'localhost' identified by 'notebook';
+	END IF;
+END 
+$
+DELIMITER ;
+CALL create_user();
 grant all on `notebook`.* to 'notebook'@'localhost';
