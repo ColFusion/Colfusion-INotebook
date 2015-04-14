3993dde43ee8b7f43d7a5f20e3971fd034ebc2dc
Jiadong Wu <wuplus1992@gmail.com>
2015-04-13 22:15:39 -0400

    update

--- MultiDBsINotebookServer/src/main/java/edu/pitt/sis/infsci2711/iNotebook/utils/AddUser.java
+++ MultiDBsINotebookServer/src/main/java/edu/pitt/sis/infsci2711/iNotebook/utils/AddUser.java
@@ -5,10 +5,13 @@
  */
 package edu.pitt.sis.infsci2711.iNotebook.utils;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -31,8 +34,18 @@ public class AddUser {
             //String s2 = "echo \"" + email + ":" + password + "\" | sudo chpasswd ";
             System.out.println("before");
             process = runtime.exec(String.format("./pleaseAddUser.sh %s %s", email, password));
+            InputStream errorStream = 
+                    process.getErrorStream();
+            String result = readStream(errorStream);
+            System.out.println("errorStream->"+result);
             System.out.println("after");
+            
+            InputStream outputStream = process.getInputStream();
+            String result2 = readStream(outputStream);
+            System.out.println("outputStream->"+result2);
+            
             process.waitFor();
+            errorStream.close();
             //System.out.println(s);
             //process = runtime.exec(s);
             //System.out.println(s2);
@@ -46,5 +59,22 @@ public class AddUser {
             e.printStackTrace();
             return false;
         }
+    }
+    
+    private static String readStream(InputStream inStream) throws IOException {
+        InputStream in = inStream;
+        InputStreamReader is = new InputStreamReader(in);
+        StringBuilder sb=new StringBuilder();
+        BufferedReader br = new BufferedReader(is);
+        String read = br.readLine();
+
+        while(read != null) {
+            //System.out.println(read);
+            sb.append(read);
+            read =br.readLine();
+
+        }
+
+        return sb.toString();
     }
 }
